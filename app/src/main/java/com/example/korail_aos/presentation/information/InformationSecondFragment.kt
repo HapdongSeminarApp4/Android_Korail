32b20445344d6e4b665ce3e4f19e51b86dd1c819
KWY <wodrk1001@gmail.com>
2022-11-24 02:36:18 +0900

    #7 [ui] second fragment 구현

--- app/src/main/java/com/example/korail_aos/presentation/information/InformationSecondFragment.kt
+++ app/src/main/java/com/example/korail_aos/presentation/information/InformationSecondFragment.kt
@@ -7,13 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.korail_aos.R
 
-class InformationFirstFragment : Fragment() {
+class InformationSecondFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.item_information_first, container, false)
+        return inflater.inflate(R.layout.item_information_second, container, false)
     }
 }
