297302f28f04a4cccb8c033a59d8b4cf73770aa8
KWY <wodrk1001@gmail.com>
2022-11-24 04:12:31 +0900

    #7 [ui, feat] page 연결

--- app/src/main/java/com/example/korail_aos/presentation/information/InformationActivity.kt
+++ app/src/main/java/com/example/korail_aos/presentation/information/InformationActivity.kt
@@ -2,11 +2,33 @@ package com.example.korail_aos.presentation.information
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.viewpager2.widget.ViewPager2
 import com.example.korail_aos.R
+import com.example.korail_aos.databinding.ActivityInformationBinding
 
 class InformationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityInformationBinding
+    private lateinit var informationAdapter: InformationAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_information)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_information)
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        informationAdapter = InformationAdapter()
+        informationAdapter.submitList(dummy)
+        binding.vpInformationPages.adapter = informationAdapter
+        binding.vpInformationPages.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+    }
+
+    companion object {
+        private val dummy =
+            listOf(Information(0), Information(1), Information(2), Information(3), Information(4))
     }
 }
+
+data class Information(
+    val id: Int
+)
