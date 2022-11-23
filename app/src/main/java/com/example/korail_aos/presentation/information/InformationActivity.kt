e8901d3fe821d519110ec08d9fb2d5e5ade155e5
KWY <wodrk1001@gmail.com>
2022-11-23 11:13:26 +0900

    #7 [chore] asset 추가

---
+++ app/src/main/java/com/example/korail_aos/presentation/information/InformationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.korail_aos.presentation.information
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.korail_aos.R
+
+class InformationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_information)
+    }
+}
