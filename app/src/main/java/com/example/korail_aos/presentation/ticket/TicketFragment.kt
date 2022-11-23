90c5553182e5daeb475cc5d005b57d3639cb9b0d
wkdyujin <yujin1203713@gmail.com>
2022-11-23 18:41:31 +0900

    [feat] viewpager 구현

--- app/src/main/java/com/example/korail_aos/presentation/ticket/TicketFragment.kt
+++ app/src/main/java/com/example/korail_aos/presentation/ticket/TicketFragment.kt
@@ -6,13 +6,52 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.korail_aos.R
+import com.example.korail_aos.databinding.FragmentTicketBinding
+import com.google.android.material.tabs.TabLayout
 
 class TicketFragment : Fragment() {
+    private var _binding: FragmentTicketBinding? = null
+    private val binding: FragmentTicketBinding
+        get() = requireNotNull(_binding) { "FragmentTicketBinding" }
+    private lateinit var tabLayout: TabLayout
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_ticket, container, false)
+        _binding = FragmentTicketBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        tabLayout = binding.tlTicketTablayout
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.ticket_verify))
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.ticket_pass))
+
+        binding.tlTicketTablayout.addOnTabSelectedListener(object :
+                TabLayout.OnTabSelectedListener {
+                // 탭의 상태가 선택으로 변경
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                }
+
+                // 탭의 상태가 선택되지 않음으로 변경
+                override fun onTabUnselected(tab: TabLayout.Tab?) {
+                }
+
+                // 이미 선택된 탭이 다시 선택됨
+                override fun onTabReselected(tab: TabLayout.Tab?) {
+                    when (tab!!.position) {
+                        0 -> TicketVerifyFragment()
+                        else -> TicketPassFragment()
+                    }
+                }
+            })
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
